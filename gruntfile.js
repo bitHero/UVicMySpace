b96e1297fdf442a2fea3186f984acd7799a71086
Ben Hawker <ben@hawker.cc>
2016-04-06 22:43:43 -0700

    Add favicon

--- gruntfile.js
+++ gruntfile.js
@@ -45,6 +45,7 @@ module.exports = function(grunt) {
           cwd: 'assets/',
           src: [
             '**/*.js',
+            '**/*.ico',
             '**/*.css',
             '**/*.css.map',
             '**/*.eot',
